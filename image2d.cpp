ca73d6d3c64294f5d9e855500bc4575a9ed4bea9
dshattuck <david.shattuck@gmail.com>
2026-03-04 09:07:29 -0800

    formatting changes

--- image2d.cpp
+++ image2d.cpp
@@ -10,30 +10,31 @@
 bool Image2D::readTiff(const std::string &ifname)
 {
   bool success = false;
-  TIFF *tif = TIFFOpen(ifname.c_str(), "r");
+  TIFF *tif=TIFFOpen(ifname.c_str(),"r");
   if (!tif) return false;
   TiffInfo info;
   info.get(tif);
-  if (info.bitsPerSample != 16 || info.samplesPerPixel != 1)
+  if (info.bitsPerSample!=16||info.samplesPerPixel!=1)
   {
-    std::cerr << "only single-channel 16-bit images are currently supported." << std::endl;
-    return false;
-  }
-  auto scanlineSize = TIFFScanlineSize(tif);
-  if (scanlineSize != sizeof(uint16_t) * info.width)
-  {
-    std::cerr << "mismatch between scanline size (" << scanlineSize << ") and image width (" << info.width << ")." << std::endl;
+    std::cerr<<"only single-channel 16-bit images are currently supported."<<std::endl;
     TIFFClose(tif);
     return false;
   }
-  width = info.width;
-  height = info.height;  
-  data.resize(width*height);
-  for (uint32_t row = 0; row < height; row++)
+  auto scanlineSize=TIFFScanlineSize(tif);
+  if (scanlineSize!=sizeof(uint16_t)*info.width)
   {
-    if (TIFFReadScanline(tif, data.data() + row * width, row, 0) == -1)
+    std::cerr<<"mismatch between scanline size ("<<scanlineSize<<") and image width ("<<info.width<<")."<<std::endl;
+    TIFFClose(tif);
+    return false;
+  }
+  width=info.width;
+  height=info.height;
+  data.resize(width*height);
+  for (uint32_t row=0;row<height;row++)
+  {
+    if (TIFFReadScanline(tif,data.data()+row*width,row,0)==-1)
     {
-      std::cerr << "error reading scanline " << row << " in file " << ifname << std::endl;
+      std::cerr<<"error reading scanline "<<row<<" in file "<<ifname<<std::endl;
       break;
     }
   }
